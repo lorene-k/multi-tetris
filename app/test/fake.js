799cfc974614d2625094cb6782ae8df7f4fb50b8
Lorene <lorenejulie@hotmail.com>
2025-12-17 14:08:16 +0100

    style: apply code formatting

--- app/test/fake.js
+++ app/test/fake.js
@@ -3,9 +3,9 @@ import chai from "chai"
 chai.should()
 
 describe('Check Sum', () => {
-  it('1+1 == 2', () => {
-    const res = 1 + 1
-    res.should.equal(2)
-  });
+    it('1+1 == 2', () => {
+        const res = 1 + 1
+        res.should.equal(2)
+    });
 
 });
