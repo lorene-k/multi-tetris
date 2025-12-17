cfaf962baf5b2b6dcf4a5360253b29f541bbdcbb
Lorene <lorenejulie@hotmail.com>
2025-12-17 16:03:00 +0100

    test(shared): add rotations tests

---
+++ app/test/shared/rotations.test.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { rotate90, rotateN } from '../../src/shared/tetris';
+import { PIECES, createEmptyBoard, createPiece } from '../../src/shared/tetris';
+
+describe('rotations.js', () => {
+
+    describe('rotate90', () => {
+        it('should preserve block count', () => {
+            Object.entries(PIECES).forEach(([type, piece]) => {
+                const rotated = rotate90(piece.shape, piece.pivot);
+                expect(rotated.length).to.equal(piece.shape.length);
+            })
+        });
+
+        it('should be immutable', () => {
+            Object.entries(PIECES).forEach(([type, piece]) => {
+                const shapeCopy = JSON.parse(JSON.stringify(piece.shape));
+                rotate90(piece.shape, piece.pivot);
+                expect(piece.shape).to.deep.equal(shapeCopy);
+            });
+        });
+    });
+
+    describe('rotateN', () => {
+        it('should return original shape after four rotations', () => {
+            Object.entries(PIECES).forEach(([type, piece]) => {
+                const rotated = rotateN(piece.shape, 4, piece.pivot);
+                expect(rotated).to.deep.equal(piece.shape);
+            });
+        });
+    });
+});
