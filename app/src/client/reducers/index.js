977df62df834de11e614ad427e41396f9c6b0571
lorene-k <lorenejulie@hotmail.com>
2026-09-04 17:45:14 +0200

    rename app -> src

--- app/src/client/reducers/index.js
+++
@@ -1,5 +0,0 @@
-import alert from './alert.js'
-export default alert
-
-
-
--- app/src/server/main.js
+++
@@ -1,6 +0,0 @@
-import { params } from '../../params.js'
-import * as server from './index.js'
-
-server.create(params.server).then(() => {
-    console.log(`Server running at http://${params.server.host}:${params.server.port}/`)
-})
